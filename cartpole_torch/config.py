6049a9b6e76cec5fa88ccc080f632c24a676726d
Nikolay Chechulin <nchechulin@protonmail.com>
2022-08-26 12:49:51 +0300

    add parameters for input delta time and dynamics updates for each input

--- cartpole_torch/config.py
+++ cartpole_torch/config.py
@@ -18,7 +18,7 @@ class SystemLimits:
 
     `max_abs_velocity` : float
         Maximum absolute velocity of the cart.
-        Measured in m/s (meters per second)
+        Measured in m/s (meters per second).
 
     `max_abs_acceleration` : float
         Maximum absolute acceleration of the cart.
@@ -45,7 +45,7 @@ class SystemParameters:
 
     `gravity` : float
         Gravitational constant, shows the gravitational pull.
-        Measured in m/s^2 (meters per second squared)
+        Measured in m/s^2 (meters per second squared).
     """
 
     pole_length: float = 0.2
@@ -56,8 +56,31 @@ class SystemParameters:
 @dataclass
 class SystemConfiguration:
     """
-    Contains physical parameters of the system and its' limits
+    Contains physical parameters of the system and its' limits.
+
+    Fields
+    ------
+    `parameters` : SystemParameters
+        Physical parameters of the system.
+
+    `limits` : SystemLimits
+        Limits of the system, such as maximum position and etc.
+
+    `input_timestep` : float
+        Time between adjusting the input of the system.
+        We assume the input is the same during the step (i.e. between two
+        timesteps).
+        Measured in seconds.
+
+    `dynamics_steps_per_input` : int
+        Shows how many times we calculate dynamics of the system during
+        one timestep.
+        We assume the input is the same during the timestep.
+        Value of 10 would mean that we update the state of the system
+        10 times before adjusting the input.
     """
 
     parameters: SystemParameters = SystemParameters()
     limits: SystemLimits = SystemLimits()
+    input_timestep: float = 1e-2
+    dynamics_steps_per_input: int = 10
