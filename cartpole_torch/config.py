7e9fdd7387a66dfe7ec92049e3224d1a9ae97197
Nikolay Chechulin <nchechulin@protonmail.com>
2022-08-26 12:31:50 +0300

    add system configuration classes

--- cartpole_torch/config.py
+++ cartpole_torch/config.py
@@ -1,3 +1,6 @@
+"""
+Defines configuration of the system - its' limits and physical parameters
+"""
 from dataclasses import dataclass
 
 
@@ -25,3 +28,36 @@ class SystemLimits:
     max_abs_position: float = 0.25
     max_abs_velocity: float = 25.0
     max_abs_acceleration: float = 7
+
+
+@dataclass
+class SystemParameters:
+    """
+    Represents physical parameters of the system
+
+    Fields
+    ------
+    `pole_length` : float
+        Length of the pole in meters.
+
+    `pole_mass` : float
+        Mass of the pole in kg.
+
+    `gravity` : float
+        Gravitational constant, shows the gravitational pull.
+        Measured in m/s^2 (meters per second squared)
+    """
+
+    pole_length: float = 0.2
+    pole_mass: float = 0.118
+    gravity: float = 9.807
+
+
+@dataclass
+class SystemConfiguration:
+    """
+    Contains physical parameters of the system and its' limits
+    """
+
+    parameters: SystemParameters = SystemParameters()
+    limits: SystemLimits = SystemLimits()
