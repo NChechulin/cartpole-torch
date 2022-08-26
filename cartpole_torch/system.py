2e225a3ae228e868a5d068fa1ba7af5fc393ccb9
Nikolay Chechulin <nchechulin@protonmail.com>
2022-08-26 13:07:55 +0300

    define a skeleton for system as a whole

---
+++ cartpole_torch/system.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from state import State
+from config import SystemConfiguration
+
+
+@dataclass
+class CartPoleSystem:
+    """
+    Defines the system as a whole
+
+    Fields
+    ------
+    `config` : SystemConfiguration
+        Physical configuration of the system.
+
+    `current_state` : State
+        Current state of the system.
+        Changes over time.
+
+    `target_state` : State
+        State we want the system to be in.
+        Should remain constant.
+
+    `simulation_time` : float
+        Time (in seconds) since the start of the simulation.
+    """
+
+    config: SystemConfiguration = SystemConfiguration()
+    current_state: State = State.home()
+    # TODO: remove field and use State.target() to ensure immutability
+    # TODO: think of cases when the field should be mutable
+    target_state: State = State.target()
+    simulation_time: float = 0.0
+
+    def advance_to(self, target_time: float) -> None:
+        """
+        Advances the system to a given moment in time.
+
+        Parameters
+        ----------
+        target_time : float
+            New time of the system.
+            Should be greater than `simulation_time`.
+
+        Raises
+        ------
+        ValueError
+            If target time is smaller than `simulation_time`.
+        """
+
+        raise NotImplementedError
+
+    def advance_one_step(self) -> None:
+        """
+        Advances the system one step further.
+        One step equals `config.input_timestep` seconds.
+        """
+
+        raise NotImplementedError
